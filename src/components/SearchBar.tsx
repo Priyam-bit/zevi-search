d5e8cd76259b8f1434740a00eb0f8216d2ed006c
priyam <btech10477.19@bitmesra.ac.in>
2023-01-06 13:15:47 +0530

    Added recommendation box popup funcionality

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,13 +2,25 @@ import './searchBar.css'
 import React from 'react'
 
 interface SearchBarProps {
-
+    setShowRecommendation? : React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export const SearchBar: React.FC<SearchBarProps> = () => {
+export const SearchBar: React.FC<SearchBarProps> = ({setShowRecommendation}) => {
+    const handleFocus = () => {
+        if (setShowRecommendation !== undefined) {
+            setShowRecommendation(true);
+        } 
+    }
+    const handleBlur = () => {
+        if (setShowRecommendation !== undefined) {
+            setShowRecommendation(false);
+        } 
+    }
         return (
             <div className="SearchBar">
                 <input 
+                    onFocus={handleFocus}
+                    onBlur = {handleBlur}
                     className='search'
                     placeholder='Search'
                     onChange={() => {}}/>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RecommendationBox } from '../components/RecommendationBox';
 import { SearchBar } from '../components/SearchBar';
 import './home.css'
@@ -8,11 +8,13 @@ interface HomeProps {
 }
 
 export const Home: React.FC<HomeProps> = () => {
+        const [showRecommendation, setshowRecommendation] = useState<boolean>(false)
+
         return (
             <div className="HomePage">
                 <div className="HomeElements">
-                    <SearchBar />
-                    <RecommendationBox />
+                    <SearchBar setShowRecommendation={setshowRecommendation} />
+                    {showRecommendation && <RecommendationBox />}
                 </div>
                 <img className='BgImage' src= "./ZeviBackground.jpg" alt='Background'/>
             </div>
