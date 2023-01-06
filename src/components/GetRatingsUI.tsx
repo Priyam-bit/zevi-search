57105aa4d8f02f4cd233c2f4c7bb4bc6ceabf637
priyam <btech10477.19@bitmesra.ac.in>
2023-01-06 19:35:23 +0530

    Added the products UI

---
+++ src/components/GetRatingsUI.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import './getRatingsUI.css'
+
+interface GetRatingsUIProps {
+    Rating : number
+}
+
+export const GetRatingsUI: React.FC<GetRatingsUIProps> = ({Rating}) => {
+    if (Rating === 5) {
+        return (
+            <span className="Stars">
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+            </span>
+        )
+    }
+    if (Rating === 4) {
+        return (
+            <span className="Stars">
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+            </span>
+        )
+    }
+    else if (Rating === 3) {
+        return (
+            <span className="Stars">
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star unchecked"></span>
+                <span className="fa fa-star unchecked"></span>
+            </span>
+        )
+    }
+    else if (Rating === 2) {
+        return (
+            <span className="Stars">
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star unchecked"></span>
+                <span className="fa fa-star unchecked"></span>
+                <span className="fa fa-star unchecked"></span>
+            </span>
+        )
+    }
+    else {
+        return (
+            <span className="Stars">
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star unchecked"></span>
+                <span className="fa fa-star unchecked"></span>
+                <span className="fa fa-star unchecked"></span>
+                <span className="fa fa-star unchecked"></span>
+            </span>
+        )
+    }
+}
--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './searchResults.css'
 import { SearchBar } from '../components/SearchBar';
+import { GetRatingsUI } from '../components/GetRatingsUI';
+import { Product } from '../Models/Product';
 
 interface SearchResultsProps {
 
 }
 
 export const SearchResults: React.FC<SearchResultsProps> = ({}) => {
+        const [products, setproducts] = useState<[Product]>();
+        
         return (
             <div className="SearchResults">
                 <div className="SearchBar">
@@ -58,58 +62,133 @@ export const SearchResults: React.FC<SearchResultsProps> = ({}) => {
                                 <div className="FilterOption">
                                     <input className='FilterCheckbox' type='radio' name='rating' id = "5*" value='5'/> 
                                     <label htmlFor="5*">
-                                        <span className="fa fa-star checked"></span>
-                                        <span className="fa fa-star checked"></span>
-                                        <span className="fa fa-star checked"></span>
-                                        <span className="fa fa-star checked"></span>
-                                        <span className="fa fa-star checked"></span>
+                                        <GetRatingsUI Rating={5}/>
                                     </label>
                                 </div>
                                 <div className="FilterOption">
                                     <input className='FilterCheckbox' type='radio' name='rating' id = "4*" value='4'/> 
                                     <label htmlFor="4*">
-                                        <span className="fa fa-star checked"></span>
-                                        <span className="fa fa-star checked"></span>
-                                        <span className="fa fa-star checked"></span>
-                                        <span className="fa fa-star checked"></span>
-                                        <span className="fa fa-star unchecked"></span>
+                                        <GetRatingsUI Rating={4}/>
                                     </label>
                                 </div>
                                 <div className="FilterOption">
                                     <input className='FilterCheckbox' type='radio' name='rating' id = "3*" value='3'/> 
                                     <label htmlFor="3*">
-                                        <span className="fa fa-star checked"></span>
-                                        <span className="fa fa-star checked"></span>
-                                        <span className="fa fa-star checked"></span>
-                                        <span className="fa fa-star unchecked"></span>
-                                        <span className="fa fa-star unchecked"></span>
+                                        <GetRatingsUI Rating={3}/> 
                                     </label>
                                 </div>
                                 <div className="FilterOption">
                                     <input className='FilterCheckbox' type='radio' name='rating' id = "2*" value='2'/> 
                                     <label htmlFor="2*">
-                                        <span className="fa fa-star checked"></span>
-                                        <span className="fa fa-star checked"></span>
-                                        <span className="fa fa-star unchecked"></span>
-                                        <span className="fa fa-star unchecked"></span>
-                                        <span className="fa fa-star unchecked"></span>
+                                        <GetRatingsUI Rating={2}/> 
                                     </label>
                                 </div>
                                 <div className="FilterOption">
                                     <input className='FilterCheckbox' type='radio' name='rating' id = "1*" value='1'/> 
                                     <label htmlFor="1*">
-                                        <span className="fa fa-star checked"></span>
-                                        <span className="fa fa-star unchecked"></span>
-                                        <span className="fa fa-star unchecked"></span>
-                                        <span className="fa fa-star unchecked"></span>
-                                        <span className="fa fa-star unchecked"></span>
+                                        <GetRatingsUI Rating={1}/>
                                     </label>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="Products">
-                        Products
+                        <div className="Product">
+                            <div className="ProductImgWrapper">
+                                <img className='ProductImg' src="./Background.jpg" alt="" />
+                            </div>
+                            <div className="ProductName">Name</div>
+                            <div className="ProductPrice">
+                                <span className='OriginalPrice'>Rs. 999</span>
+                                <span className='DiscountedPrice'> Rs.899</span>
+                            </div>
+                            <div className="ProductRating">
+                                <span className='RatingImg'>
+                                    <GetRatingsUI Rating={5}/>                                    
+                                </span>
+                                <span className='Reviews'>(20)</span>
+                            </div>
+                        </div>
+                        <div className="Product">
+                            <div className="ProductImgWrapper">
+                                <img className='ProductImg' src="" alt="" />
+                            </div>
+                            <div className="ProductName">Name</div>
+                            <div className="ProductPrice">
+                                <span className='OriginalPrice'>Rs. 999</span>
+                                <span className='DiscountedPrice'> Rs.899</span>
+                            </div>
+                            <div className="ProductRating">
+                                <span className='RatingImg'>
+                                    <GetRatingsUI Rating={5}/>                                    
+                                </span>
+                                <span className='Reviews'>(20)</span>
+                            </div>
+                        </div>
+                        <div className="Product">
+                            <div className="ProductImgWrapper">
+                                <img className='ProductImg' src="" alt="" />
+                            </div>  
+                            <div className="ProductName">Name</div>
+                            <div className="ProductPrice">
+                                <span className='OriginalPrice'>Rs. 999</span>
+                                <span className='DiscountedPrice'> Rs.899</span>
+                            </div>
+                            <div className="ProductRating">
+                                <span className='RatingImg'>
+                                    <GetRatingsUI Rating={5}/>                                    
+                                </span>
+                                <span className='Reviews'>(20)</span>
+                            </div>
+                        </div>
+                        <div className="Product">
+                            <div className="ProductImgWrapper">
+                                <img className='ProductImg' src="" alt="" />
+                            </div>  
+                            <div className="ProductName">Name</div>
+                            <div className="ProductPrice">
+                                <span className='OriginalPrice'>Rs. 999</span>
+                                <span className='DiscountedPrice'> Rs.899</span>
+                            </div>
+                            <div className="ProductRating">
+                                <span className='RatingImg'>
+                                    <GetRatingsUI Rating={5}/>                                    
+                                </span>
+                                <span className='Reviews'>(20)</span>
+                            </div>
+                        </div>
+                        <div className="Product">
+                            <div className="ProductImgWrapper">
+                                <img className='ProductImg' src="" alt="" />
+                            </div>
+                            <div className="ProductName">Name</div>
+                            <div className="ProductPrice">
+                                <span className='OriginalPrice'>Rs. 999</span>
+                                <span className='DiscountedPrice'> Rs.899</span>
+                            </div>
+                            <div className="ProductRating">
+                                <span className='RatingImg'>
+                                    <GetRatingsUI Rating={5}/>                                    
+                                </span>
+                                <span className='Reviews'>(20)</span>
+                            </div>
+                        </div>
+                        <div className="Product">
+                            <div className="ProductImgWrapper">
+                                <img className='ProductImg' src="" alt="" />
+                            </div>
+                            <div className="ProductName">Name</div>
+                            <div className="ProductPrice">
+                                <span className='OriginalPrice'>Rs. 999</span>
+                                <span className='DiscountedPrice'> Rs.899</span>
+                            </div>
+                            <div className="ProductRating">
+                                <span className='RatingImg'>
+                                    <GetRatingsUI Rating={5}/>                                    
+                                </span>
+                                <span className='Reviews'>(20)</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
